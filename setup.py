ce1ecc4f693a3ea21bc04c993e8056ee22bc137e
Mohamed Boussakouk <m.boussakouk@student.bit-academy.nl>
2021-03-01 00:15:52 +0100

    Removed FTP option

--- setup.py
+++ setup.py
@@ -4,27 +4,10 @@ def lampStackSetup():
     lampCommands = open("LAMP-commands.txt", "r")
 
     for command in lampCommands:
-        # os.system( command );
+        os.system( command );
         print(command)
-
-def ftpServerSetup():
-    ftpCommands = open("FTP-commands.txt", "r")
-
-    for command in ftpCommands:
-        # os.system( command );
-        print(command)
-
-# def f(x):
-#     return {
-#         'a': test(),
-#         'b': 2,
-#     }[x]
 
 lamp = input('Setup LAMP Stack install (Yes or No): ').lower()
-ftp = input('Setup FTP acces (Yes or No): ').lower()
 
 if lamp == 'yes':
     lampStackSetup();
-elif ftp == 'yes':
-    ftpServerSetup();
-
